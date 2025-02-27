c801c5e31f816abcec6556dbda75a98c3b96c576
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-27 16:11:07 +0100

    add array and cycle for generated mail

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,21 @@
+/*Descrizione
+Attraverso l'apposita API di Boolean generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Abbellire con CSS o Bootstrap
+Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)*/
+
 //axios
 axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
   .then(response => {
-    console.log(response);
     console.log(response.data.response);
+    let emailList = [];
 
+    for(i = 0; i < 1; i++){
+        emailList.push(response.data.response);
+    }
+    
+    console.log(emailList)
   })
   .catch(error => console.error(error));
+
+
